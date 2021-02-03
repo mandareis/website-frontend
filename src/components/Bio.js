47b87146633d3c8a42d1050e7f205dd8729e8a16
mandareis <amanda@depaula.xyz>
2021-02-03 07:44:05 -0800

    minor fixes to css and bio div

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -7,12 +7,25 @@ const Bio = () => {
     <div>
       <div className="inner-container-bio">
         <img src={avatar} alt="avatar" className="avatar center"></img>
-        <h2 id="full-name">Amanda de Paula</h2>
-        <p id="bio-p">(she/her)</p>
-        <p id="bio-p">
+        <div id="full-name">
+          <p>Amanda de Paula </p>
+        </div>
+
+        <div id="bio-p">
+          <p id="bio-p">(she/her)</p>
           Junior software engineer based in San Francisco, Ca. My passions
-          include coffee, React, chocolate and fine wine.
-        </p>
+          include coffee, React.js, chocolate and fine wine.
+          <div className="current-project" style={{ textAlign: "center" }}>
+            <p>Ongoing project:</p>
+            <a
+              href="https://exploretravel.tips"
+              rel="noreferrer"
+              target="_blank"
+            >
+              www.exploretravel.tips
+            </a>
+          </div>
+        </div>
         <div id="contact-container">
           <Link to="/contact" id="contact-btn">
             Connect with me <i className="fas fa-chevron-circle-right"></i>
